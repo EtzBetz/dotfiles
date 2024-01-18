37b5421b439ae35385eb174b321f29ec2d32f80a
Raphael Betz <raphaelbetz@gmail.com>
2024-01-18 17:13:34 +0100

    add desktop backgrounds, automatically set desktop background

--- macos/install.sh
+++ macos/install.sh
@@ -12,3 +12,6 @@
 ./defaults.sh
 ./hostname.sh
 ./keyboard.sh
+
+file="$HOME/.dotfiles/macos/backgrounds/arun-clarke-UDEb05r0Uy8-unsplash.jpg"
+osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"${file}\" as POSIX file"
