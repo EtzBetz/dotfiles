2c02da50060fb345bda790b2d62a6d43b2a3d458
Raphael Betz <raphaelbetz@gmail.com>
2025-08-06 22:37:49 +0200

    chmod update

--- install.sh
+++ install.sh
@@ -12,6 +12,13 @@ echo "Please accept the administrator prompt for setup."
 # Ask for the administrator password upfront
 sudo -v
 
+chmod +x ./homebrew/install.sh
+chmod +x ./zsh/install.sh
+chmod +x ./git/install.sh
+chmod +x ./macos/install.sh
+chmod +x ./iterm/install.sh
+chmod +x ./python/install.sh
+
 ./homebrew/install.sh
 ./zsh/install.sh
 ./git/install.sh
